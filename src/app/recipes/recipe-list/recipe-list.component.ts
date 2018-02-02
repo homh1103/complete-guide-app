62438defe63fdc1ddab8faabbc95b84555277315
homh1103 <homh1103@gmail.com>
2018-02-02 17:01:10 -0500

    homh

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model'
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes : Recipe[] = [
+    new Recipe('Recipe', 'Recipe description',
+      'https://static01.nyt.com/images/2016/02/16/dining/16COOKING-SALMONWITHLEEKS2/16COOKING-SALMONWITHLEEKS2-articleLarge.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
